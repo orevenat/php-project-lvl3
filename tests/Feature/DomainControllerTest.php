d907ab253d3284b67675df3fab0d7e36002e94d6
Ruslan Knyazev <agrry@yandex.ru>
2020-07-18 20:00:16 +0500

    add test for DomainController

---
+++ tests/Feature/DomainControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+
+class DomainControllerTest extends TestCase
+{
+    private $id;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->id = DB::table('domains')->insertGetId(
+            ['name' => 'https://yandex.ru']
+        );
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('domains.index'));
+        $response->assertOk();
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('domains.show', $this->id));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $data = ['name' => 'https://testpage.com'];
+        $response = $this->post(route('domains.store'), ['domain' => $data]);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('domains', $data);
+    }
+}
